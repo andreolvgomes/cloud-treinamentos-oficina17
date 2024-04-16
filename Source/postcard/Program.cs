07a09074a4ea39d55c9abf527b2e801f6975b3d7
andreolvgome <inforoliver@gmail.com>
2024-04-16 08:53:52 -0300

    Algumas mudanças necessárias:
    XML encryptor configured. Key {80435ee6-52e1-4fe3-81db-837ad268fc48} may be persisted to storage in unencrypted form.

--- Source/postcard/Program.cs
+++ Source/postcard/Program.cs
@@ -1,7 +1,18 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDataProtection().UseCryptographicAlgorithms(
+    new AuthenticatedEncryptorConfiguration
+    {
+        EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
+        ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
+    });
 
 var app = builder.Build();
 
